d20b451e03aab642727ac4076627e62fe1605fd0
runage <me@runage.dev>
2024-07-02 01:40:02 +0200

    config setup

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "endonix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/default/configuration.nix
+        # inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
